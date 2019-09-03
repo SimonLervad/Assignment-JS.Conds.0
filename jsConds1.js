ede559370b0aff5f333eb691134d43ffc24ca028
lervad96 <54528992+lervad96@users.noreply.github.com>
2019-09-03 13:54:31 +0200

    Add files via upload

---
+++ jsConds1.js
@@ -0,0 +1,173 @@
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+let t1 = 0;
+let t2 = 0;
+let t3 = 0;
+let t4 = 0;
+let t5 = 0;
+let t6 = 0;
+let flip;
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
+
+flip = play(6);
+if (flip === 1)
+    t1++;
+else if (flip === 2)
+    t2++;
+else if (flip === 3)
+    t3++;
+else if (flip === 4)
+    t4++;
+else if (flip === 5)
+    t5++;
+else if (flip === 6)
+    t6++;
+
+console.log(flip);
